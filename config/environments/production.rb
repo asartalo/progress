33e3c90f5cbc93bd4eb71752830d3c9f2512bb8f
Wayne Duran <asartalo@gmail.com>
2019-04-10 19:37:52 +0800

    Fix problem with production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  url = URL(ENV['APP_URL'])
+  url = URI(ENV['APP_URL'])
   config.action_mailer.default_url_options = { host: url.host }
 
   # Ignore bad email addresses and do not raise email delivery errors.

f08e21349369b68ab04df9d94dae3e7bb53deae5
Wayne Duran <asartalo@gmail.com>
2019-04-11 09:46:36 +0800

    Fix problem with production scss compilation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,7 +4,7 @@ const { environment } = require('@rails/webpacker')
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader',
   options: {
-    attempts: 1
+    // attempts: 1
   }
 });
 
